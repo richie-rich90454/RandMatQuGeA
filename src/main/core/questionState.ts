a69b42d814cfa217e19718e07a132b067dc25db9
richie-rich90454 <rjiang880@gmail.com>
2026-06-25 12:57:22 -0700

    Mirror questionState to window globals

--- src/main/core/questionState.ts
+++ src/main/core/questionState.ts
@@ -1,30 +1,36 @@
-import type { CorrectAnswer } from "../../types/global.d.ts";
+import type { CorrectAnswer } from "../../types/global";
 export class QuestionState{
-    private _correctAnswer: CorrectAnswer;
-    private _expectedFormat: string;
-    private _hasQuestion: boolean;
-    constructor(){
-        this._correctAnswer={ correct: "", alternate: "", display: "" };
-        this._expectedFormat="";
-        this._hasQuestion=false;
-    }
-    get correctAnswer(): CorrectAnswer{
-        return this._correctAnswer;
-    }
-    set correctAnswer(value: CorrectAnswer){
-        this._correctAnswer=value;
-    }
-    get expectedFormat(): string{
-        return this._expectedFormat;
-    }
-    set expectedFormat(value: string){
-        this._expectedFormat=value;
-    }
-    get hasQuestion(): boolean{
-        return this._hasQuestion;
-    }
-    set hasQuestion(value: boolean){
-        this._hasQuestion=value;
-    }
+	private _correctAnswer: CorrectAnswer;
+	private _expectedFormat: string;
+	private _hasQuestion: boolean;
+	constructor(){
+		this._correctAnswer={ correct: "", alternate: "", display: "" };
+		this._expectedFormat="";
+		this._hasQuestion=false;
+		window.correctAnswer=this._correctAnswer;
+		window.expectedFormat=this._expectedFormat;
+		window.hasQuestion=this._hasQuestion;
+	}
+	get correctAnswer(): CorrectAnswer{
+		return this._correctAnswer;
+	}
+	set correctAnswer(value: CorrectAnswer){
+		this._correctAnswer=value;
+		window.correctAnswer=value;
+	}
+	get expectedFormat(): string{
+		return this._expectedFormat;
+	}
+	set expectedFormat(value: string){
+		this._expectedFormat=value;
+		window.expectedFormat=value;
+	}
+	get hasQuestion(): boolean{
+		return this._hasQuestion;
+	}
+	set hasQuestion(value: boolean){
+		this._hasQuestion=value;
+		window.hasQuestion=value;
+	}
 }
 export const questionState=new QuestionState();
