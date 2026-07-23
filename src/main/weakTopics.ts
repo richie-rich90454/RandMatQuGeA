7b29ae01cd165a73ce54650c7abd5b3dcc57b655
richie-rich90454 <rjiang880@gmail.com>
2026-07-24 07:33:17 +0800

    format weakTopics.ts imports consistently

--- src/main/weakTopics.ts
+++ src/main/weakTopics.ts
@@ -4,10 +4,10 @@
  * @description Fetches weak topics from backend and displays a modal with practice buttons.
  * Now triggered manually by the user via the "Recommend Topics" button.
  */
-import {invoke} from"@tauri-apps/api/core";
-import {selectTopic} from"./topics";
-import {topics} from"./constants";
-import {generateQuestion} from"./generation";
+import{invoke}from"@tauri-apps/api/core";
+import{selectTopic}from"./topics";
+import{topics}from"./constants";
+import{generateQuestion}from"./generation";
 import*as settings from"./settings";
 import*as ui from"./ui";
 import {appState} from"./core/stateStore";
