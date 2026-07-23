5d5792e23d127621102aa9d2d469048b948ee36d
richie-rich90454 <rjiang880@gmail.com>
2026-07-24 07:58:54 +0800

    export getPendingCount from barrel index

--- src/main/index.ts
+++ src/main/index.ts
@@ -7,7 +7,7 @@ export{topicRegistry,registerTopic}from"./services/topicRegistry";
 export type{TopicEntry}from"./services/topicRegistry";
 export{bindEvents,countBindings,hasBinding}from"./services/eventBinder";
 export type{EventBinding}from"./services/eventBinder";
-export{evaluateInWorker,simplifyInWorker,parseInWorker,terminateWorker,isWorkerAvailable}from"./services/mathWorkerClient";
+export{evaluateInWorker,simplifyInWorker,parseInWorker,terminateWorker,isWorkerAvailable,getPendingCount}from"./services/mathWorkerClient";
 export{showQuestionSkeleton,hideQuestionSkeleton,isSkeletonActive,getLastSkeletonTime}from"./ui/skeleton";
 export{offlineIndicator,OfflineIndicator}from"./ui/offlineIndicator";
 export{initVirtualGrid,refreshVirtualGrid,getVisibleRange,getTotalItems}from"./ui/virtualTopicGrid";
