e5d33c7a20a91f2bdb9943a4cc85709cb97a061b
richie-rich90454 <rjiang880@gmail.com>
2026-07-27 16:18:00 +0800

    move: relocate modules/Geometry/GeometryArea.test.ts to src/__tests__/

--- src/__tests__/modules/Geometry/GeometryArea.test.ts
+++ src/__tests__/modules/Geometry/GeometryArea.test.ts
@@ -2,8 +2,8 @@
  * @vitest-environment jsdom
  */
 import {describe,it,expect} from "vitest";
-import {generateAreaCircle,generateAreaRectangle,generateAreaTriangle,generateSectorArea,generateSurfaceAreaCube} from "./GeometryArea.js";
-import {seededRng} from "../../main/core/Rng";
+import {generateAreaCircle,generateAreaRectangle,generateAreaTriangle,generateSectorArea,generateSurfaceAreaCube} from "../../../modules/Geometry/GeometryArea.js";
+import {seededRng} from "../../../main/core/Rng";
 describe("generateAreaCircle",()=>{
 	it("returns a QuestionDto with required fields",()=>{
 		const dto=generateAreaCircle("medium", seededRng(42));
