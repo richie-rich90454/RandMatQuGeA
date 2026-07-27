c44ec5817e70baa7c87cb87c4811f14f2aac0568
richie-rich90454 <rjiang880@gmail.com>
2026-07-27 16:17:51 +0800

    move: relocate modules/Algebra/AlgebraUtils.test.ts to src/__tests__/

--- src/__tests__/modules/Algebra/AlgebraUtils.test.ts
+++ src/__tests__/modules/Algebra/AlgebraUtils.test.ts
@@ -1,5 +1,5 @@
 ﻿import {describe,it,expect} from "vitest";
-import {factorial,gcd,getOrdinal,getMaxForDifficulty} from "./AlgebraUtils.js";
+import {factorial,gcd,getOrdinal,getMaxForDifficulty} from "../../../modules/Algebra/AlgebraUtils.js";
 describe("factorial",()=>{
     it("returns 1 for 0",()=>{
         expect(factorial(0)).toBe(1);
