996cb0fb7a8df4afe3ba09e7ffc4081d0748513f
richie-rich90454 <rjiang880@gmail.com>
2026-07-27 16:17:50 +0800

    move: relocate main/core/Rng.test.ts to src/__tests__/

--- src/__tests__/main/core/Rng.test.ts
+++ src/__tests__/main/core/Rng.test.ts
@@ -1,5 +1,5 @@
 ﻿import { describe, it, expect } from "vitest";
-import { seededRng, randInt, pick, shuffle } from "./Rng";
+import { seededRng, randInt, pick, shuffle } from "../../../main/core/Rng";
 describe("seededRng", ()=>{
 	it("returns values in [0, 1)", ()=>{
 		let rng = seededRng(12345);
