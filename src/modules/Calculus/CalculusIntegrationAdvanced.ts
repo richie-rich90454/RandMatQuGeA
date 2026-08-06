33cc672248126b5d1533afd33a2b618c1f6eb89d
richie-rich90454 <rjiang880@gmail.com>
2026-08-06 10:51:35 +0800

    cleanup: remove unused latexToPlain import and its ts-expect-error

--- src/modules/Calculus/CalculusIntegrationAdvanced.ts
+++ src/modules/Calculus/CalculusIntegrationAdvanced.ts
@@ -1,6 +1,5 @@
 ﻿import type {RngFn, QuestionDto} from "../../types/global";
-// @ts-expect-error - latexToPlain is imported for potential future use
-import {getMaxCoeff, latexToPlain} from "./CalculusUtils.js";
+import {getMaxCoeff} from "./CalculusUtils.js";
 /**
  * Generates a random advanced integration or differential equations question.
  * Includes custom multiple‑choice options for MCQ mode.
